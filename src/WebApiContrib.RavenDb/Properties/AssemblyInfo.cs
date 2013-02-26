13bfaf0122e8eb4363f2a87c4423274e47570d2c
Chris Missal <chris.missal@gmail.com>
2013-02-26 01:08:58 -0600

    moved 'beta' into nuspec file

--- src/WebApiContrib.RavenDb/Properties/AssemblyInfo.cs
+++ src/WebApiContrib.RavenDb/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.9.3-beta")]
-[assembly: AssemblyFileVersion("0.9.3-beta")]
+[assembly: AssemblyVersion("0.9.3")]
+[assembly: AssemblyFileVersion("0.9.3")]
